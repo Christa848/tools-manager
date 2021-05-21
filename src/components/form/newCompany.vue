b609c5f7b52b5c9936906db2171bfc85e44fb9a8
Christa848 <78590853+Christa848@users.noreply.github.com>
2021-05-21 13:20:23 +0200

    update v2

--- src/components/form/newCompany.vue
+++ src/components/form/newCompany.vue
@@ -1,130 +1,254 @@
 <template>
-       <div class="px-5 py-2">
+
+  <div id="tabb">
+<b-row >
+  <b-col cols="2">
+    
+    </b-col>
+   
+      <div>
         
+    
+    
+      
+<div>
+    <b-form @submit="createContact" @reset="onReset" v-if="show">
 
 
-  <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Company:"
-        label-for="input-1"
-        
-      >
-      <b-form-select
-        
-          id="input-1"
-          v-model="form.contact"
-          :options="[{ text: 'Choose...', value: null }, 'One', 'Two', 'Three']"
-          placeholder="First Name"
+      <b-form-group id="input-group-2" label=" Company Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="Name"
+          placeholder=" name"
           required
-        >
-      </b-form-select>
+        ></b-form-input>
       </b-form-group>
-      <b-form-group 
-         label="Region:"
-         id="input-group-2"
-         label-for="input-2">
-      <b-form-select
-      v-model="form.subject"
-      id="inline-form-custom-select-pref"
-      class="mb-2 mr-sm-2 mb-sm-0"
-      :options="[{ text: 'Subject...', value: null }, 'One', 'Two', 'Three']"
-      :value="null"
-    ></b-form-select>
 
-      </b-form-group>
-   <b-card>
-      <b-form-group
-        id="input-group-1"
-        label="contact:"
-        label-for="input-1"
-        
-      >
-      <b-form-select
-        
-          id="input-1"
-          v-model="form.contact"
-          :options="[{ text: 'Choose...', value: null }, 'One', 'Two', 'Three']"
-          placeholder="First Name"
+          <b-form-group id="input-group-4" label="Region:" label-for="input-2">
+        <b-form-input
+          id="input-4"
+          v-model="Region"
+          placeholder=" region"
           required
-        >
-      </b-form-select>
-      </b-form-group>
-      <b-form-group
-         label="address:"
-         id="input-group-2"
-         label-for="input-2">
-      <b-form-select
-      v-model="form.subject"
-      id="inline-form-custom-select-pref"
-      class="mb-2 mr-sm-2 mb-sm-0"
-      :options="[{ text: 'Subject...', value: null }, 'One', 'Two', 'Three']"
-      :value="null"
-    ></b-form-select>
-
+        ></b-form-input>
       </b-form-group>
 
-       <b-form-group
-         label="email:"
-         id="input-group-2"
-         label-for="input-2">
-      <b-form-select
-      v-model="form.subject"
-      id="inline-form-custom-select-pref"
-      class="mb-2 mr-sm-2 mb-sm-0"
-      :options="[{ text: 'Subject...', value: null }, 'One', 'Two', 'Three']"
-      :value="null"
-    ></b-form-select>
-
+      <b-form-group id="input-group-3" label="Contact:" label-for="input-2">
+        <b-form-input
+          id="input-3"
+          v-model="Contact"
+          placeholder="contact"
+          required
+        ></b-form-input>
       </b-form-group>
-   </b-card>
 
-      <b-button type="submit" variant="primary">Create</b-button>
+     
+
+            <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="Email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+           <b-form-group id="input-group-6" label="Adress:" label-for="input-2">
+        <b-form-input
+          id="input-6"
+          v-model="Adress"
+          placeholder="adress"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      
+
+      <b-button type="submit" variant="primary">Submit</b-button>
       <b-button type="reset" variant="danger">Reset</b-button>
     </b-form>
-   
+    
   </div>
 
 
-        
-      </div>
-</template>
-<script>
-export default {
-     data() {
-      return {
-        form: {
-          contact: '',
-          name: '',
-          food: null,
-        
-        },
-        foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
-        show: true
-      }
+
+
       
+      
+   
+  </div>
+
+   
+    </b-row>
      
   
+
+
+  
+</div>
+</template>
+<script>
+ import axios from 'axios';
+   export default{
+
+  data (){ return{
+    filter: "",
+    data: []
+
+    ,
+
+    form: {
+          email: '',
+          name: '',
+          food: null,
+          checked: []
+        },
+        show: true,
+
+         Name:"",
+          Region:"",
+          Contact:"",
+          Adress:"",
+          Email:""
+    
+
+         
+
+    
+      }
+              
+        
+      
+  },
+ beforeMount(){
+    this.getName();
+  },
+  methods: {
+    async getName(){
+      const res = await fetch('http://itrackdevs.geo-fuel.com/tools_manager_api/getComcontacts.php');
+      const data = await res.json();
+      this.data = data;
     },
-     methods: {
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
-      },
-      onReset(event) {
+
+    createContact: function(){
+        console.log("Create contact!")
+
+        let formData = new FormData();
+        console.log("Name:", this.Name),
+        console.log("Region:", this.Region),
+        console.log("Contact:", this.Contact),
+        console.log("Email:", this.Email),
+        console.log("Adress:", this.Adress),
+        
+        
+
+        formData.append('Name', this.Name),
+        formData.append('Region', this.Region),
+        formData.append('Contact', this.Contact),
+        formData.append('Email', this.Email),
+        formData.append('Adress', this.Adress)
+        
+        
+           
+        var contact = {};
+        formData.forEach(function(value, key){
+            contact[key] = value;
+        });
+
+        axios({
+            method: 'post',
+            url: 'http://itrackdevs.geo-fuel.com/tools_manager_api/companiesCont.php',
+            data: formData,
+            config: { headers: {'Content-Type': 'multipart/form-data' }}
+        })
+       
+        .catch(function (response) {
+            //handle error
+            console.log(response)
+            
+        });
+    },
+
+
+
+            onReset(event) {
         event.preventDefault()
         // Reset our form values
-        this.form.contact = ''
-        this.form.name = ''
-        this.form.food = null
-        this.form.checked = []
+        this.Email = ''
+        this.Name = ''
+        this.Address 
+        
         // Trick to reset/clear native browser form validation state
         this.show = false
         this.$nextTick(() => {
           this.show = true
         })
-      }
-    }
-}
+            },
+
+
+  
+   
+  },
+  
+ 
+};
+
 </script>
+
+<style scoped>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+th {
+  background-color: #dddddd;
+}
+
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid lawngreen;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 25px;
+}
+#srch{
+  width: 180px;
+}
+#not{
+  margin-top: 25px;
+}
+</style> -->
+
+
+
+
+
+<style>
+#app{
+  margin-top: 10px;
+}
+#back{
+  margin-left: 98%;
+  margin-top: 2px;
+}
+
+</style>
+
+
